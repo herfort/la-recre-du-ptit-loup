3410f3ee60b9e501d2f65ab4d7e1ca390309804d
herfort <vamp290@hotmail.com>
2026-07-07 09:01:12 +0200

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -6,10 +6,30 @@ const SUPABASE_URL = "https://jbialegbayusckjjajnq.supabase.co";
 const SUPABASE_KEY =
 "sb_publishable__buDYmorRpoNxxjFGq56Iw_28SaX6IG";
 
-
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
 
 console.log("✅ Connexion à Supabase réussie");
+
+
+// ===============================
+// Test lecture table shooting_parametres
+// ===============================
+
+async function chargerParametresShooting() {
+
+  const { data, error } = await supabaseClient
+    .from("shooting_parametres")
+    .select("*");
+
+  if (error) {
+    console.error("❌ Erreur lecture shooting_parametres :", error);
+    return;
+  }
+
+  console.log("✅ Données shooting_parametres récupérées :", data);
+}
+
+chargerParametresShooting();
