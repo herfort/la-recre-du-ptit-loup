bc3995cb3d9d5f3330221f5a5b409b1b1715ee19
herfort <vamp290@hotmail.com>
2026-07-08 17:37:44 +0200

    Update admin.js

--- admin-shooting/admin.js
+++ admin-shooting/admin.js
@@ -1,1 +1,103 @@
-console.log("✅ Administration Shooting chargée");
+// ===============================
+// Connexion Supabase
+// ===============================
+
+const SUPABASE_URL =
+"https://jbialegbayusckjjajnq.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable__buDYmorRpoNxxjFGq56Iw_28SaX6IG";
+
+const supabaseClient =
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
+console.log("✅ Administration connectée à Supabase");
+
+// ===============================
+// Chargement des inscriptions
+// ===============================
+
+async function chargerInscriptions(){
+
+const { data, error } =
+await supabaseClient
+.from("shooting_inscriptions")
+.select("*")
+.order("creneau");
+
+if(error){
+
+console.error(error);
+return;
+
+}
+
+console.log(data);
+
+afficherInscriptions(data);
+
+}
+
+// ===============================
+// Affichage du tableau
+// ===============================
+
+function afficherInscriptions(data){
+
+const tbody =
+document.getElementById(
+"listeInscriptions"
+);
+
+tbody.innerHTML = "";
+
+if(data.length === 0){
+
+tbody.innerHTML = `
+<tr>
+<td colspan="5">
+Aucune inscription
+</td>
+</tr>
+`;
+
+return;
+
+}
+
+data.forEach(inscription=>{
+
+const enfants =
+inscription.enfants
+.map(e=>e.prenom+" "+e.nom)
+.join("<br>");
+
+tbody.innerHTML += `
+
+<tr>
+
+<td>${inscription.creneau}</td>
+
+<td>
+${inscription.prenom_parent}
+${inscription.nom_parent}
+</td>
+
+<td>${enfants}</td>
+
+<td>${inscription.type_photo}</td>
+
+<td>${inscription.duree} min</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+chargerInscriptions();
